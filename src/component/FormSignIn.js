16c988fff8779d178f15dcedbd34cdb0082200e6
unknown <nongdiem00@gmail.com>
2020-10-18 23:59:02 +0700

    update form

--- src/component/FormSignIn.js
+++ src/component/FormSignIn.js
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 import "./FormSignIn.css";
 
 export default class Login extends Component {
-    render() {
-        return (
-            <form class="form-sign-in">
-                <h1>Sign in</h1>
-                <input placeholder="Email" type="email" />
-                <input placeholder="Password" type="password" />
-                <button>Sign in</button>
-                <a href="#link">Forgot Password?</a>
-                <div class="create-acc">
-                    <p>Don't have an account? </p>
-                    <a href="#link">Create one</a>
-                </div>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form class="form-sign-in">
+        <h1>Sign in</h1>
+        <input placeholder="Email" type="email" required=""/>
+        <input placeholder="Password" type="password" required=""/>
+        <button>Sign in</button>
+        <a href="#link">Forgot Password?</a>
+        <div class="create-acc">
+          <p>Don't have an account? </p>
+          <a href="#link">Create one</a>
+        </div>
+      </form>
+    );
+  }
 }
--- src/component/FormSignUp.js
+++ src/component/FormSignUp.js
@@ -6,10 +6,10 @@ export default class Login extends Component {
         return (
             <form class="form-sign-up">
                 <h1>Sign up</h1>
-                <input placeholder="Username" type="text"/>
-                <input placeholder="Email" type="email" />
-                <input placeholder="Password" type="password"/>
-                <input placeholder="Confirm Password" type="password"/>
+                <input placeholder="Username" type="text" required=""/>
+                <input placeholder="Email" type="email" required=""/>
+                <input id="pass" placeholder="Password" type="password"/>
+                <input id="cf-pass" placeholder="Confirm Password" type="password"/>
                 <button>Sign up</button>
                 <div class="go-sign-in">
                     <p>Already have an account? </p>
