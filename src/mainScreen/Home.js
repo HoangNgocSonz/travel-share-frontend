de7159b589072ff5f9319ec099b2ffb1e6937a06
HoangNgocSonz <hoangson20bg@gmail.com>
2020-10-20 16:01:38 +0700

    update navbar, add login login

--- src/mainScreen/Home.js
+++ src/mainScreen/Home.js
@@ -9,7 +9,6 @@ import FormSignIn from "../component/FormSignIn";
 import FormSignUp from "../component/FormSignUp";
 import PostDetail from "../component/PostDetail";
 import UserDetail from "../component/UserDetail";
-import AboutUs from "../component/AboutUs";
 
 export default class Home extends Component {
   constructor(props) {
